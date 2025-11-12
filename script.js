d3683db42fc9c01b852ae642dab86fbee6dc4f13
Paulo <paulo2renato88@gmail.com>
2025-11-12 11:21:01 -0300

    Melhorias na UX e melhoria na função Primeira Letra Máiúscula

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* Programa para manipular o DOM */
 
+
 window.document.addEventListener( "DOMContentLoaded", function () {
     document.getElementById("btnMaiuscula").addEventListener("click", function () {
         let inputText = document.getElementById("input-text").value
@@ -12,8 +13,15 @@ window.document.addEventListener( "DOMContentLoaded", function () {
     })
 
     document.getElementById("btnPrimeira").addEventListener("click", function () {
-        let inputText = document.getElementById("input-text").value
-        document.getElementById("resultado").innerHTML = inputText[0].toUpperCase() + inputText.slice(1).toLowerCase()
+        let inputText = document.getElementById("input-text").value;
+
+        let lowerText = inputText.toLowerCase();
+
+        let resultText = lowerText.replace(/(^|\. *)([a-z])/g, function(match, p1, p2) {
+        return p1 + p2.toUpperCase();
+        });
+
+        document.getElementById("resultado").innerHTML = resultText;
     })
 
     document.getElementById("btnLimpar").addEventListener("click", function () {
